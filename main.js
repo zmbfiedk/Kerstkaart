295e9614b2408f3cdbc2e165014f8924a0ee52dc
Arthur <lotuslovesurmom@gmail.com>
2024-11-18 12:14:24 +0100

    center en horizontal lines

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const canvas = document.getElementById("canvasId");
 const g = canvas.getContext("2d");
 
 function drawCandyBackground() {
-    const stripeWidth = 40;
-    const stripeColor1 = "#FF0000";
-    const stripeColor2 = "#FFFFFF";
+    const stripeHeight = 40; 
+    const stripeColor1 = "#FF0000"; 
+    const stripeColor2 = "#FFFFFF"; 
 
-    for (let i = 0; i < canvas.width; i += stripeWidth * 2) {
+    for (let i = 0; i < canvas.height; i += stripeHeight * 2) {
         g.fillStyle = stripeColor1;
-        g.fillRect(i, 0, stripeWidth, canvas.height);
+        g.fillRect(0, i, canvas.width, stripeHeight);
         g.fillStyle = stripeColor2;
-        g.fillRect(i + stripeWidth, 0, stripeWidth, canvas.height);
+        g.fillRect(0, i + stripeHeight, canvas.width, stripeHeight);
     }
 }
 
