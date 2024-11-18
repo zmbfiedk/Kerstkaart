e5352fa721cec8c91cb0715318b8c1992d022c97
Arthur <lotuslovesurmom@gmail.com>
2024-11-18 12:05:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,28 +3,15 @@ const canvas = document.getElementById("canvasId");
 const g = canvas.getContext("2d");
 
 function drawCandyBackground() {
-    const stripeWidth = 20; 
-    const stripeColor1 = "#FF0000"; 
+    const stripeWidth = 40;
+    const stripeColor1 = "#FF0000";
     const stripeColor2 = "#FFFFFF";
 
-    for (let i = -canvas.height; i < canvas.width; i += stripeWidth * 2) {
-        g.beginPath();
-        g.moveTo(i, 0);
-        g.lineTo(i + stripeWidth, 0);
-        g.lineTo(i + stripeWidth + canvas.height, canvas.height);
-        g.lineTo(i + canvas.height, canvas.height);
-        g.closePath();
-        g.fillStyle = stripeColor1; 
-        g.fill();
-
-        g.beginPath();
-        g.moveTo(i + stripeWidth, 0);
-        g.lineTo(i + stripeWidth * 2, 0);
-        g.lineTo(i + stripeWidth * 2 + canvas.height, canvas.height);
-        g.lineTo(i + stripeWidth + canvas.height, canvas.height);
-        g.closePath();
+    for (let i = 0; i < canvas.width; i += stripeWidth * 2) {
+        g.fillStyle = stripeColor1;
+        g.fillRect(i, 0, stripeWidth, canvas.height);
         g.fillStyle = stripeColor2;
-        g.fill();
+        g.fillRect(i + stripeWidth, 0, stripeWidth, canvas.height);
     }
 }
 
